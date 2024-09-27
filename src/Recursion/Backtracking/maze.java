acc7e25fbf8e11ec0f3c5558ef9e69d6a7f464ee
agharsh2653 <agrawalharsh2653@gmail.com>
2024-09-27 23:43:29 +0530

    maze problem with all parameters

--- src/Recursion/Backtracking/maze.java
+++ src/Recursion/Backtracking/maze.java
@@ -7,6 +7,19 @@ public class maze {
         System.out.println(count(2,2));
         path("",2,2);
         System.out.println(pathRet("",2,2));
+        System.out.println(pathRetDiagonal("",2,2));
+        boolean[][] board = {
+                {true,true,true},
+                {true,false,true},
+                {true,true,true}
+        };
+        boolean[][] board2 = {
+                {true,true,true},
+                {true,true,true},
+                {true,true,true}
+        };
+        pathRestriction("",board,0,0);
+        addAllPath("",board2,0,0);
     }
 
     static int count(int r, int c){
@@ -48,6 +61,71 @@ public class maze {
             list.addAll(pathRet(p+"R",r,c-1));
         }
         return list;
+    }
+    static ArrayList<String> pathRetDiagonal(String p, int r, int c){
+        if(r==1 && c==1){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+
+        ArrayList<String> list = new ArrayList<>();
+        if(r>1 && c>1){
+            list.addAll(pathRet(p+"D",r-1,c-1));
+        }
+        if(r>1){
+            list.addAll(pathRet(p+"V",r-1,c));
+        }
+        if(c>1){
+            list.addAll(pathRet(p+"H",r,c-1));
+        }
+        return list;
+    }
+
+    static void pathRestriction(String p,boolean[][] maze, int r, int c){
+        if(r== maze.length-1 && c==maze[0].length-1){
+            System.out.println(p);
+            return;
+        }
+
+        if(!maze[r][c]){
+            return;
+        }
+        if(r<maze.length-1){
+            pathRestriction(p+"D",maze,r+1,c);
+        }
+        if(c<maze[0].length-1){
+            pathRestriction(p+"R",maze,r,c+1);
+        }
+    }
+
+    static void addAllPath(String p,boolean[][] maze, int r, int c){
+        if(r== maze.length-1 && c==maze[0].length-1){
+            System.out.println(p);
+            return;
+        }
+
+        if(!maze[r][c]){
+            return;
+        }
+
+        // i am considering this black in my path
+        maze[r][c]=false;
+        if(r<maze.length-1){
+            addAllPath(p+"D",maze,r+1,c);
+        }
+        if(c<maze[0].length-1){
+            addAllPath(p+"R",maze,r,c+1);
+        }
+        if(r>0){
+            addAllPath(p+"U",maze,r-1,c);
+        }
+        if(c>0){
+            addAllPath(p+"L",maze,r,c-1);
+        }
+        //this line is where the function will be over
+        //so before the function gets  removed, also remove the changes that were made by that function
+        maze[r][c]=true;
     }
 
 }
